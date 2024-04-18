52e164c979554c18175c577d9ed4efc4745cbd2c
slam814 <abdussalam6111997@gmail.com>
2024-04-18 19:52:58 +0600

    my first commit

---
+++ loop.js
@@ -0,0 +1,16 @@
+// loop
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+
+
+// sum 
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    const num = i;
+    sum = sum + num;
+    console.log(i, sum)
+
+}
---
+++ var-let-constent.js
@@ -0,0 +1,50 @@
+// var: no reason to use var
+
+// var is backdeted
+
+// let : allow it to reassign
+// const: do not allow it to reassign
+
+
+
+const money = 20;
+
+// money = 25;
+
+console.log(money);
+
+let myMoney = 5000;
+
+myMoney = 6000;
+myMoney = 70000;
+console.log(myMoney);
+
+
+const myArray = [12, 13, 14, 15, 16];
+
+myArray.push(12, 33, 44, 55)
+
+console.log(myArray);
+
+let myLetArray = [ 12, 44, 55, 66, 777];
+
+myLetArray = [111]
+
+console.log(myLetArray);
+
+myArray.pop();
+console.log(myArray);
+myLetArray.pop();
+console.log(myLetArray);
+
+// Object 
+const student ={
+name: 'Alia Vat',
+class: 12,
+
+}
+
+student.name = 'alia chodry'
+console.log(student);
+
+
